11d33ed04e28b93dbadbc8e3b84a3a7bee3927d4
Katya Prymak <62486148+katyaprymak@users.noreply.github.com>
2024-01-12 17:54:38 +0100

    js cat changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var button = document.querySelector('.show-cat');
-console.log("button");
+//console.log("button");
 
 
 var cat = document.querySelector('cat');
-console.log("cat");
+//console.log("cat");
+
+button.addEventListener("click", function () {
+    cat.classList.add("show");
+});
